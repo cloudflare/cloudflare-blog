8d98e68802d57668d187d90d4e5b7b5a755a1feb
Jakub Sitnicki <jakub@cloudflare.com>
2022-09-30 13:04:11 +0200

    2022-10: BPF tail calls without secrets

---
+++ 2022-10-bpf-tail-call/tail_call_ex2.bpf.c
@@ -0,0 +1,32 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#define __unused __attribute__((unused))
+
+char __license[] SEC("license") = "GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+	__uint(max_entries, 1);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, sizeof(__u32));
+} jmp_table SEC(".maps");
+
+SEC("tc")
+int target_prog(struct __sk_buff *skb __unused)
+{
+	return 0xcafe;
+}
+
+static __noinline
+int sub_func(struct __sk_buff *skb)
+{
+	bpf_tail_call(skb, &jmp_table, 0);
+	return 0xf00d;
+}
+
+SEC("tc")
+int entry_prog(struct __sk_buff *skb)
+{
+	__attribute__((musttail)) return sub_func(skb);
+}
